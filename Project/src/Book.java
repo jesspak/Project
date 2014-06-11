6d2e707e1096b96531fa62d83665e25f9c2282f4
Tom8515 <tombaudin650@gmail.com>
2014-06-11 10:49:24 -0700

    Update Book.java

--- Project/src/Book.java
+++ Project/src/Book.java
@@ -26,7 +26,16 @@ public class Book extends Media implements Serializable{ //JB
 	 */
 	public Book(String author, String title, 
 			String format, String location, String notes){
-		super();
+	/**
+         * calls super consturctor of Media class and passes paaramaters
+         */
+				 
+		super(title, format, location, notes);
+		
+		/**
+		 *passes author to setAuthor method to set the author of book
+		 */
+		setAuthor(author);
 	}
 	/**
 	 * returns the current value of bookAuthor
