fbbb09084bdd63644878565d9cc57042cc190c90
Tom8515 <tombaudin650@gmail.com>
2014-06-14 22:35:31 -0700

    Update Library.java

--- Project/src/Library.java
+++ Project/src/Library.java
@@ -1,70 +1,298 @@
 import java.util.ArrayList; // JB
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;//JB
+
 /**
- * one object of this class creates a library and has methods 
- * that adds, prints, deletes, and retrieves the data.
+ * one object of this class creates a library and has methods that adds, prints,
+ * deletes, and retrieves the data.
  */
-public class Library implements Serializable{
+public class Library implements Serializable {
 	
+//	static File MasterDB_F = new File ("library.db");
+
 	private ArrayList<Media> library;
+
 	/**
-	 * default constructor
-	 *JP
+	 * default constructor JP
 	 */
-	public Library(){
-		
-	}
-	/**
-	 * adds a book to the library
-	 * JB
-	 */
-	public void add(Media m){
-		library.add(m);
+	public Library() {
+
 	}
 	
 	/**
-	 * sorts the all the entries in the list
-	 * JP
+	 * Loads object ContactList from file and returns it to the variable.
+	 * @author TB
 	 */
-	public void sort(){
-		
+	public static Library loadFromDisk() {
+		Library library = null;
+		System.out.println("Checking for previously saved library.");
+		try {
+			FileInputStream inFile = new FileInputStream("library");
+			ObjectInputStream inObject = new ObjectInputStream(inFile);
+			library = (Library) inObject.readObject();
+			inFile.close();
+			inObject.close();
+			System.out.println("**Library found and loaded**\n");
+		} 
+		catch (IOException ioe) {
+			System.out.println("Error reading from the file: " + ioe.getMessage());
+		} 
+		catch (ClassNotFoundException cnfe) {
+			System.out.println("Error in casting to Library: " + cnfe);
+		}
+		return library;
+	}
+	
+	/**
+	 * Saves object Library to the file.
+	 * @author TB
+	 */
+	public static void saveToDisk(Media library) {
+		try {
+			FileOutputStream outFileStream = new FileOutputStream("library");
+			ObjectOutputStream outObjectStream= new ObjectOutputStream(outFileStream);
+			outObjectStream.writeObject(library);
+			outFileStream.close();
+			outObjectStream.close();
+			System.out.println("Entry successfully saved to file");
+		} 
+		catch (IOException anIOException) {   
+		    System.out.println(anIOException);
+		}
 	}
 
 	/**
-	 * takes in the media type as a parameter and searches
-	 * the list of that same media type
-	 * JP
+	 * adds a form of media to the library TB
 	 */
-	public Media retrieveByMedia(Media m){
-		return new Media();//JB
+	public void add(Media m) {
+		library.add(m);
+		
+	    saveToDisk(m);
 	}
+
 	/**
-	 * takes in the title as a parameter and searches
-	 * the list of that same title
-	 * JP
+	 * sorts the all the entries in the list JP
 	 */
-	public Media retrieveByTitle(Media title){
-		return new Media();//JB
-	}
+	public void sort(){
+		ArrayList<Media> bookList = new ArrayList<Media>();
+		for (int i=0; i< library.size(); i++)
+		{
+			if (library.get(i) instanceof Book)
+			{
+				if (bookList.size() == 0) 
+					bookList.add(library.get(i));
+				else 
+					for (int j = 0; j < bookList.size(); j++)
+					{
+						if ((library.get(i).getTitle().compareTo(bookList.get(j).getTitle())) < 0)
+						{
+							bookList.add(j, library.get(i));
+							break;
+						}
+						else
+							bookList.add(library.get(i));
+					}
+			}
+		}
+
+		ArrayList<Media> songList = new ArrayList<Media>();
+		for (int i2=0; i2 < library.size(); i2++)
+		{
+			if (library.get(i2) instanceof Song)
+			{
+				if (songList.size() == 0) 
+				songList.add(library.get(i2));
+			else
+				for (int j2 = 0; j2 < songList.size(); j2++)
+				{
+					if ((library.get(i2).getTitle().compareTo(songList.get(j2).getTitle())) < 0)
+					{
+						songList.add(j2, library.get(i2));
+						break;
+					}
+						else
+							songList.add(library.get(i2));
+
+				}
+
+			}
+		}	
+
+		
+		ArrayList<Media> videoList = new ArrayList<Media>();
+		for (int i3=0; i3 < library.size(); i3++)
+		{
+			if(library.get(i3) instanceof Video)
+			{
+				if (videoList.size() == 0) 
+					videoList.add(library.get(i3));
+				else
+					for (int j3 = 0; j3 < videoList.size(); j3++)
+					{
+						if ((library.get(i3).getTitle().compareTo(videoList.get(j3).getTitle())) < 0)
+						{
+							videoList.add(j3, library.get(i3));
+							break;
+						}
+						else
+							videoList.add(library.get(i3));
+					}
+			}
+		}
+
+		ArrayList<Media> videoGameList = new ArrayList<Media>();
+		for (int i4=0; i4< library.size(); i4++)
+		{
+			if(library.get(i4)  instanceof VideoGame)
+			{
+				if (videoGameList.size() == 0) 
+					videoGameList.add(library.get(i4));
+				else
+					for (int j4 = 0; j4 < videoGameList.size(); j4++)
+					{
+						if ((library.get(i4).getTitle().compareTo(videoGameList.get(j4).getTitle())) < 0)
+						{
+							videoGameList.add(j4, library.get(i4));
+							break;
+						}
+						else
+							videoGameList.add(library.get(i4));
+
+					}
+
+			}
+		}
+
+
+		ArrayList<Media> sortedArrayList = new ArrayList<Media>();
+			sortedArrayList.addAll(bookList);
+			sortedArrayList.addAll(songList);
+			sortedArrayList.addAll(videoList);
+			sortedArrayList.addAll(videoGameList);
+
+		library = sortedArrayList;
+		}
+
+
 	/**
-	 * takes in the media type and title as a parameter and searches
-	 * the list of that same media type and title
-	 * JP
+	 * takes in the media type as a parameter and searches the list of that same
+	 * media type JP
 	 */
-	public Media retrieveByTitleAndMedia(Media tm){
-		return new Media();//JB
+	// public Media retrieveByMedia(Media m){
+	// int i= 0;
+	// Media curMedia;
+	// for (i = 0; i < library.size(); i++){
+	// curMedia = library.get(i);
+	// if(curMedia == title){
+	// break;
+	// }
+	// }
+	// return curMedia;
+
+	public void retriveByMedia(String type){
+
+		if (type == "books")
+			searchInBooks();
+		else if (type == "songs")
+			searchInSongs();
+		else if (type == "videos")
+			searchInVideos();
+		else if (type == "video games")
+			searchInVideogames();
+		
 	}
+
+	public String searchInBooks() {
+		ArrayList<Media> bookList = new ArrayList<Media>();
+		for (int i = 0; i < library.size(); i++) {
+			if (library.get(i) instanceof Book) {
+				bookList.add(library.get(i));
+			}
+		}
+
+		return bookList.toString();
+	}
+
+	public String searchInSongs() {
+		ArrayList<Media> songList = new ArrayList<Media>();
+		for (int i = 0; i < library.size(); i++) {
+			if (library.get(i) instanceof Song) {
+				songList.add(library.get(i));
+			}
+		}
+		return songList.toString();
+	}
+
+	public String searchInVideos() {
+		ArrayList<Media> videoList = new ArrayList<Media>();
+		for (int i = 0; i < library.size(); i++) {
+			if (library.get(i) instanceof Video) {
+				videoList.add(library.get(i));
+			}
+		}
+		return videoList.toString();
+	}
+
+	public String searchInVideogames() {
+		ArrayList<Media> videoGameList = new ArrayList<Media>();
+		for (int i = 0; i < library.size(); i++) {
+			if (library.get(i) instanceof VideoGame) {
+				videoGameList.add(library.get(i));
+			}
+		}
+		return videoGameList.toString();
+	}
+
+	/**
+	 * takes in the title as a parameter and searches the list of that same
+	 * title JP
+	 */
+	public String retrieveByTitle(String title) {
+		String curMedia = "";
+		int i = 0;
+		for (i = 0; i < library.size(); i++) {
+			curMedia = library.get(i).getTitle();
+			if (curMedia == title) {
+				break;
+			}
+		}
+		return curMedia;
+	}
+
+	/**
+	 * takes in the media type and title as a parameter and searches the list of
+	 * that same media type and title JP
+	 */
+	public Media retrieveByTitleAndMedia(Media tm) {
+		return new Media();// JB
+	}
+
 	/**
 	 * deletes an entry from an ArrayList JB
 	 */
-	public void deleteEntry(){ // JP
-		
+	public void deleteEntry(Media title) { // JP
+		int i = 0;
+		for (i = 0; i < library.size(); i++) {
+			Media curMedia = library.get(i);
+			if (curMedia == title) {
+				break;
+			}
+		}
+		library.remove(i);
 	}
+
 	/**
-	 * 
+	 * Returns a string containing all the data stored in this object JP
 	 */
 	public String toString(){
-		return null;
+		String result = " ";
+		for(int i = 0; i < library.size(); i++)
+			result += " " + library.get(i);
+		return result;
 	}
-	
 }
