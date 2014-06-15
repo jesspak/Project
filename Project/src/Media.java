fd450d6ec85027df9b7f1825963ba24f580607ce
Tom8515 <tombaudin650@gmail.com>
2014-06-14 22:35:52 -0700

    Update Media.java

--- Project/src/Media.java
+++ Project/src/Media.java
@@ -1,4 +1,3 @@
-
 import java.io.Serializable;
 /**
  * One object of class Media stores a list of the various
@@ -30,48 +29,37 @@ public class Media implements Serializable, Cloneable  { //JB,JP
 	 * @param notes
 	 */
 
-	public Media(String title, String format, // JB
-			String location, String notes) {// JP
-			
-		title = this.title;
-		format = this.format;
-		location = this.location;
-		notes = this.notes;
+	public Media(String newTitle, String newFormat, // JB
+			String newLocation, String newNotes) {// JP
+		title = newTitle;
+		format = newFormat;
+		location = newLocation;
+		notes = newNotes;
 	}
-	
-
 	/**
 	 * returns the current value of title//JB
 	 */
-
 	public String getTitle() {// JB
 		return title;// JB
 	}
-
 	/**
 	 * returns the current value of format//JP
 	 */
-
 	public String getFormat() {// JP
 		return format;// JP
 	}
-
 	/**
 	 * returns the current value of location//JP
 	 */
-
 	public String getLocation() {// JP
 		return location;// JP
 	}
-
 	/**
 	 * returns the current value of notes//JP
 	 */
-
 	public String getNotes() {// JP
 		return notes;// JP
 	}
-
 	/**
 	 * sets the value of title to newTitle//JB
 	 * 
@@ -79,10 +67,9 @@ public class Media implements Serializable, Cloneable  { //JB,JP
 	 *            //JB
 	 */
 
-	public void setTitle(String title) {// JB
-		this.title = title;// JB
+	public void setTitle(String newTitle) {// JB
+		title = newTitle;// JB
 	}
-
 	/**
 	 * sets the value of format to newFormat//JP
 	 * 
@@ -90,44 +77,44 @@ public class Media implements Serializable, Cloneable  { //JB,JP
 	 *            //JP
 	 */
 
-	public void setFormat(String format) {// JP
-		this.format = format;
-
+	public void setFormat(String newFormat) {// JP
+		format = newFormat;// JP
+	}
 	/**
 	 * sets the value of location to newLocation//JP
 	 * 
 	 * @param newLocation
 	 */
-
-	public void setLocation(String location) {// JP
-		this.location = location;// JP
+public void setLocation(String newLocation) {// JP
+		location = newLocation;// JP
 	}
-
 	/**
 	 * sets the value of notes to newNotes//JP
 	 * 
 	 * @param newNotes
 	 */
 
-	public void setNotes(String notes) {// JP
-		this.notes = notes;// JP
+	public void setNotes(String newNotes) {// JP
+		notes = newNotes;// JP
 	}
-
 	/**
-	 * Returns a String containing all the data stored //JP in this object.
+	 * Returns a String containing all the data stored in this object. JP
 	 */
-
 	public String toString() {// JP
-		return this.getTitle() + "\n" + this.getFormat() + "\n"
-				+ this.getLocation()// JB
-				+ "\n" + this.getNotes() + "\n";// JP
+		return "Title: " + this.getTitle() + "\n" + "Format: " + this.getFormat() + "\n"
+				+ "Location: " + this.getLocation()// JB
+				+ "\n" + "Notes: " + this.getNotes() + "\n" + "\n";// JP
 	}
 
 	/**
 	 * Compares entries to allow them to be sorted.//JB
 	 */
-	public int compareTo(Object arg0) {// JB
-		return 0;// JB
-	}
+//	public int compareTo(Media m) {// JB
+//		int compareQuantity = ((Media) compareTitle).getQuantity(); 
+//		 
+//		//ascending order
+//		return this.quantity - compareQuantity;
+//
+//	}
 
 }
